9096c4965f20db6f77b296dfddd6ab5b3d0daf8e
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-05-11 14:16:44 -0500

    Added header exposure to plugin callbacks

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var addCorsHeaders = require('hapi-cors-headers')
+var addCorsHeaders    = require('hapi-cors-headers'),
+    addExposedHeaders = require('./lib/expose-headers')
 var pkg = require('./package.json')
 
 function corsPlugin (server, options, next) {
   server.ext('onPreResponse', addCorsHeaders)
+  server.ext('onPreResponse', addExposedHeaders)
   next()
 }
+
 
 corsPlugin.attributes = {
   pkg: pkg
